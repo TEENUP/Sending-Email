2427660e41444ec75cacab06513a1ca8d34bc06f
Puneet <puneet@macb.tftus.com>
2017-07-25 12:07:30 +0530

    email functionality implemented

--- sendingEmail/sendingEmail/ViewController.swift
+++ sendingEmail/sendingEmail/ViewController.swift
@@ -5,11 +5,43 @@
 //  Created by Puneet on 25/07/17.
 //  Copyright © 2017 PuneetGupta. All rights reserved.
 //
-
+import Foundation
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
+    @IBAction func sendButtonTapped(_ sender: AnyObject) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail(){
+          self.present(mailComposeViewController, animated: true, completion: nil)
+        }
+        else{
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController{
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+       mailComposerVC.setToRecipients(["gupts.puneet@tftus.com"])
+       mailComposerVC.setSubject("Sending you an in-app e-mail")
+        mailComposerVC.setMessageBody("Sending an in-app e-mail nt so bad!!!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert(){
+        let sendMailErrorAlert = UIAlertView(title:"Could not send email", message:"Your device could not send email. please check email configuration and try again.", delegate:self, cancelButtonTitle:"Ok")
+        sendMailErrorAlert.show()
+        
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
